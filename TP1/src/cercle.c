82931f8c60c66b8d9520394783318b232d2a01ed
mulumbamagloire <mulumbamagloire57@gmail.com>
2025-10-02 12:23:20 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,1 @@
-#include <stdio.h>   
-#include <math.h>    
 
-int main() {
-    double pi = 3.14159; 
-    double rayon = 6.0;
-    printf("L'air du cercle =%f, pi*(rayon*rayon)");
-    printf("Le perimetre du cercle =%f, pi*(rayon+rayon)");
-    }
-}
