22bb7814965bb931955a24d445ecd74fd2f15746
mulumbamagloire <mulumbamagloire57@gmail.com>
2025-10-02 16:31:34 +0200

    Update puissance.c

---
+++ TP2/src/puissance.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int a ;
+    int b ;
+    int c = 1;
+
+    for(int i = 1; i <= b; i++) {
+        c *= a;
+    }
+    printf("%d", c);
+    return 0;       
+}
